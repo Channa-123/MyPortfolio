83f739364b6e919d1ed15fd3f126ef258fba1f6c
Channa-123 <96125389+Channa-123@users.noreply.github.com>
2024-03-22 23:26:04 +0530

    Create Navbar.jsx

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import './Navbar.css'
+import Toggles from '../Toggles/Toggles'
+import {Link} from 'react-scroll'
+
+function Navbar  ()  {
+  return (
+    <div className='n-wrapper'>
+        <div className='n-left'>
+            <div className='n-name'>Channa</div>
+            <Toggles />
+        </div>
+        <div className='n-right'>
+            <div className='n-list'>
+                <ul style={{listStyleType:'none'}}>
+                    <Link spy={true} to='Navbar' smooth={true} activeClass="activeClass">
+                      <li>Home</li>
+                    </Link>
+                    <Link spy={true} to='Services' smooth={true} >
+                    <li>Services</li>
+                    </Link>
+                    <Link spy={true} to='Experience' smooth={true}> 
+                    <li>Experience</li>
+                    </Link>
+                    <Link spy={true} to='Portfolio' smooth={true}>
+                    <li>Projects</li>
+                    </Link>
+                    <Link spy={true} to='Testimonials' smooth={true} >
+                    <li>Testimonials</li>
+                    </Link>
+                    
+                </ul>
+            </div>
+            <button className='button  n-button'>
+            < Link spy={true} to='Contact' id='Footer' smooth={true}>
+                Contact
+            </Link>
+            </button>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Navbar
