6dbc2fa29df47578c737d4a7f799f9b9d691f66c
Channa-123 <96125389+Channa-123@users.noreply.github.com>
2024-03-25 01:50:20 +0530

    Create Experience.jsx

---
+++ src/Components/Experience/Experience.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import './Experience.css'
+import { themeContext } from '../../Context';
+import { useContext } from "react";
+
+
+const Experience = () => {
+
+  const theme =useContext(themeContext);
+  const darkMode = theme.state.darkMode;
+  return (
+    <div className='experience' id='Experience'>
+     <div className='archivement'>
+        <div className='circle' style={{color: darkMode?'var(--orange)':''}}>8+</div>
+        <span style={{color: darkMode?'white':''}}>Years</span>
+        <span>Experience</span>
+     </div>
+     <div className='archivement'>
+        <div className='circle' style={{color: darkMode?'var(--orange)':''}}>10+</div>
+        <span style={{color: darkMode?'white':''}}>Completed</span>
+        <span>Projects</span>
+     </div>
+     <div className='archivement'>
+        <div className='circle' style={{color: darkMode?'var(--orange)':''}}>5+</div>
+        <span style={{color: darkMode?'white':''}}>Companies</span>
+        <span>Work</span>
+     </div>
+    </div>
+  )
+}
+
+export default Experience
