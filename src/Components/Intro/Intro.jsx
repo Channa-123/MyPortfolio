1e7492ffe946f101352646450261fb34aa509f14
Channa-123 <96125389+Channa-123@users.noreply.github.com>
2024-03-26 00:30:25 +0530

    Update Intro.jsx

--- src/Components/Intro/Intro.jsx
+++ src/Components/Intro/Intro.jsx
@@ -5,7 +5,7 @@ import LinkedIn from '../../img/linkedin.png';
 import Instagram from '../../img/instagram.png';
 import Vector1 from '../../img/Vector1.png';
 import Vector2 from '../../img/Vector2.png';
-import boy from '../../img/boy.png';
+//import boy from '../../img/boy.png';
 import thumbup from '../../img/thumbup.png';
 import Crown from '../../img/crown.png';
 import glassesimoji from '../../img/glassesimoji.png';
@@ -13,7 +13,8 @@ import FlotingDiv from '../FloatingDiv/FloatingDiv';
 import { themeContext } from '../../Context';
 import { useContext } from "react";
 import {motion} from 'framer-motion'
-//import myimg from '../../img/myimg.jpg';
+import me from '../../img/me.png';
+import { Link } from 'react-scroll';
 
 const Intro = () => {
   const transition = {duration : 2, type: 'spring'}
@@ -26,8 +27,11 @@ const Intro = () => {
         <div className='i-left'>
         <div className='i-name'>
             <span style={{color: darkMode? 'white': '' }}>Hy! I Am </span>
-            <span>Channa Prasad</span>
-            <span>I am an undergraduate student specializing in software 
+            <span>Channa Prasad</span> <span>
+            University of Sri Jayewardenepura <br/>
+            Bachelor of Information andCommunication Technology <br/>
+            2021 - Present</span>
+            <span><h2> I am an undergraduate student specializing in software 
               technology at the University of Sri Jayawardenepura. 
               I have learned programming languages such as Java, Python, 
               C++, and JavaScript, and I am passionate about using technology 
@@ -35,9 +39,10 @@ const Intro = () => {
               database management and software engineering principles, I thrive 
               in collaborative environments and enjoy contributing to innovative 
               projects while continually expanding my skills.
+              </h2>
             </span>
         </div>
-        <button className="button i-button">Hire me</button>
+        <button className="button i-button">< Link spy={true} to='Contact' id='Footer' smooth={true}> Hire me </Link></button>
         <div className='i-icons'>
             <a href="https://github.com/Channa-123"> 
             <img src={Github} alt="" /></a>
@@ -50,7 +55,9 @@ const Intro = () => {
       <div className='i-right'>
         <img src={Vector1} alt="" />
         <img src={Vector2} alt="" />
-        <img src={boy} alt="" />
+        <img src={me} alt="" />
+        
+
         <motion.img
         initial={{left: '-36%'}}
         whileInView={{left:'-24%'}}
@@ -68,10 +75,10 @@ const Intro = () => {
         </motion.div>
 
         <motion.div 
-        initial={{left: '9rem', top:'18rem'}}
-        whileInView={{left: '0rem'}}
+        initial={{left: '9rem', top:'22rem'}}
+        whileInView={{left: '-5rem'}}
         transition={transition}
-        style={{top: '18rem', left:'0rem'}}
+        style={{top: '22rem', left:'0rem'}}
         className='floating-div'
         >
             <FlotingDiv image={thumbup} txt1="Best Design" txt2="Award"/>
