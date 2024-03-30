0fafbce932eb5e28adef6f589c89c51022e989b0
Channa-123 <96125389+Channa-123@users.noreply.github.com>
2024-03-31 00:17:14 +0530

    Create Testimonials.jsx

---
+++ src/Components/Testimonials/Testimonials.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Testimonials.css'
+
+function Testimonials() {
+  return (
+    <table className='pagiTable'>
+    <tr>
+      <td>&lt;</td>
+      <td>1</td>
+      <td>2</td>
+      <td>3</td>
+      <td>4</td>
+      <td>5</td>
+      <td>&gt;</td>
+    </tr>
+    </table>
+  )
+}
+
+export default Testimonials
