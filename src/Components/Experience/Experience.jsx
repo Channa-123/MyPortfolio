bcf351a42526a8b6866bdbfdf175f2415a3e8269
Channa-123 <96125389+Channa-123@users.noreply.github.com>
2025-11-20 00:14:36 +0530

    Update Experience.jsx

--- src/Components/Experience/Experience.jsx
+++ src/Components/Experience/Experience.jsx
@@ -1,35 +1,3 @@
-// import React from 'react'
-// import './Experience.css'
-// import { themeContext } from '../../Context';
-// import { useContext } from "react";
-
-
-// const Experience = () => {
-
-//   const theme =useContext(themeContext);
-//   const darkMode = theme.state.darkMode;
-//   return (
-//     <div className='experience' id='Experience'>
-//      <div className='archivement'>
-//         <div className='circle' style={{color: darkMode?'var(--orange)':''}}>2+</div>
-//         <span style={{color: darkMode?'white':''}}>My</span>
-//         <span>Certificates</span>
-//      </div>
-//      <div className='archivement'>
-//         <div className='circle' style={{color: darkMode?'var(--orange)':''}}>10+</div>
-//         <span style={{color: darkMode?'white':''}}>Completed</span>
-//         <span>Projects</span>
-//      </div>
-//      <div className='archivement'>
-//         <div className='circle' style={{color: darkMode?'var(--orange)':''}}>2+</div>
-//         <span style={{color: darkMode?'white':''}}>Years of</span>
-//         <span>Experience</span>
-//      </div>
-//     </div>
-//   )
-// }
-
-// export default Experience
 
 import React from 'react';
 import './Experience.css';
@@ -42,8 +10,7 @@ const Experience = () => {
   const darkMode = theme.state.darkMode;
 
   return (
-    <div className="experience-wrapper" id="Experience">
-      {/* === Achievement Circles === */}
+<div className={`experience-wrapper ${darkMode ? 'dark' : ''}`} id="Experience">      {/* === Achievement Circles === */}
       <div className="experience">
         <div className="archivement">
           <div className="circle">2+</div>
@@ -73,38 +40,41 @@ const Experience = () => {
 
         <div className="timeline">
           {/* First Experience */}
+          <div className style={{marginLeft: '-8rem'  }}> 
+            <div className='timeline-1'>
           <div className="timeline-item" data-aos="fade-right" data-aos-delay="100">
-            <div className="timeline-content">
-              <h3>Intern Software Engineer</h3>
-              <p>
-                FagginApps Pvt Ltd. (NeuraIT) | July 2024 - Feb 2025
-                <span className="location">
-                  <FaLocationDot /> SLIIT Innovate, Malabe
-                </span>
-              </p>
-              <ul>
-                <li>Developed scalable web apps using React.js, Node.js & MongoDB</li>
-                <li>Worked closely with senior engineers, UI/UX, QA & PM teams</li>
-                <li>Actively participated in Agile sprints and delivered features</li>
-              </ul>
+               <div className ="timeline-content">
+               <h3>Intern Software Engineer</h3>
+               <p>
+                  FagginApps Pvt Ltd. (NeuraIT) | July 2024 - Feb 2025
+                  <span className="location">
+                     <FaLocationDot /> SLIIT Innovate, Malabe
+                  </span>
+               </p>
+               <ul>
+                  <li>Developed scalable web apps using React.js, Node.js & MongoDB</li>
+                  <li>Worked closely with senior engineers, UI/UX, QA & PM teams</li>
+                  <li>Actively participated in Agile sprints and delivered features</li>
+               </ul>
+               </div>
+            </div>
             </div>
           </div>
 
           {/* Second Experience */}
           <div className="timeline-item" data-aos="fade-right" data-aos-delay="300">
+            <div className='timeline-2'>
             <div className="timeline-content">
-              <h3>Frontend Developer</h3>
-              <p>
-                Freelance | Mar 2023 - Jun 2024
-                <span className="location">
-                  <FaLocationDot /> Remote
-                </span>
-              </p>
+              
+              
               <ul>
-                <li>Built responsive websites & web apps with React, Tailwind CSS</li>
-                <li>Optimized performance, debugging and client communication</li>
-                <li>Delivered 10+ successful projects with great feedback</li>
+                <li>Supported the improvement of software features through coding, debugging, testing
+                activities and Developed frontend features.</li>
+                <li>Gained practical experience using industry-standard tools and technologies to deliver clientfocused
+                solutions.</li>
+                <li>Contributed to code reviews and documentation.</li>
               </ul>
+            </div>
             </div>
           </div>
         </div>
